f263e2b7d30321231ae4804db974154104adb372
mathjken <ogujohnkennedy@gmail.com>
2023-04-19 22:21:34 +0000

    task 4 and 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
-"""BasicCache module
-"""
+
+""" Basic Dictionary """
+
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """A simple cache that stores key-value pairs in a dictionary
-    Args:
-        BaseCaching (class): Basic class for this class
-    """
+    """Basic dictionary"""
 
     def put(self, key, item):
-        """Add a new key-value pair to the cache dictionary
-        Args:
-            key ([type]): key of dictionary self.cache_data
-            item ([type]): value of key
-        """
-        if key and item:
+        """Puts item in cache"""
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """Return the value associated with the given key from the cache dictionary
-        Args:
-            key ([type]): key to search into cache_data
-        """
-        if not key or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        """Gets item from cache"""
+        return self.cache_data.get(key, None)
+
