e70df7c2e8ce69be614676ee396f7c50c9f4a570
mathjken <ogujohnkennedy@gmail.com>
2023-04-19 22:18:26 +0000

    corrections

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,42 +1,48 @@
 #!/usr/bin/python3
-"""FIFOCache module
-"""
+""" FIFO Caching """
+
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """FIFOCache class
-    Args:
-        BaseCaching (class): Basic class for this class
-    """
+    """ FIFO caching """
 
     def __init__(self):
+        """ Constructor """
         super().__init__()
-        self.__keys = []
+        self.queue = []
 
     def put(self, key, item):
-        """Add a new key-value pair to the cache dictionary and the list
-        of keys using the FIFO algorithm. If the maximum number of items
-        is reached, remove the oldest item from the dictionary and the list
-        of keys.
-        Args:
-            key ([type]): key of dictionary
-            item ([type]): item to insert in dictionary
-        """
-        if len(self.cache_data) == self.MAX_ITEMS and key not in self.__keys:
-            discd = self.__keys.pop(0)
-            del self.cache_data[discard]
-            print('DISCARD: {}'.format(discd))
-        if key and item:
-            self.__keys.append(key)
-            self.cache_data[key] = item
+        """ Puts item in cache """
+        if key is None or item is None:
+            return
+
+        if key not in self.queue:
+            self.queue.append(key)
+        else:
+            self.mv_last_list(key)
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first = self.get_first_list(self.queue)
+            if first:
+                self.queue.pop(0)
+                del self.cache_data[first]
+                print("DISCARD: {}".format(first))
 
     def get(self, key):
-        """Get the value associated with the given key from the cache
-        dictionary, or None if the key is not in the dictionary.
-        Args:
-            key ([type]): key to search into cache_data
-        """
-        if not key or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        """ Gets item from cache """
+        return self.cache_data.get(key, None)
+
+    def mv_last_list(self, item):
+        """ Moves element to last idx of list """
+        length = len(self.queue)
+        if self.queue[length - 1] != item:
+            self.queue.remove(item)
+            self.queue.append(item)
+
+    @staticmethod
+    def get_first_list(array):
+        """ Get first element of list or None """
+        return array[0] if array else None
