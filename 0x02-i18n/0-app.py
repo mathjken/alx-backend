af008c887decbbe248f3a27d2cfeae01661debae
mathjken <ogujohnkennedy@gmail.com>
2023-04-25 22:12:11 +0000

    task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+    The basic flask app module
+'''
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def helloWorld() -> str:
+    '''
+        home index page function.
+    '''
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
